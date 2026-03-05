da251905e74b8c443871869673f117f328656682
thibault-clara <thibault.clara@outlook.com>
2026-03-05 16:12:54 +0100

    conf.py fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,37 +1,38 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+import os
+import sys
+
+# Adjust this so it points to your package root from docs/source/
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'mavion-drl-ppo'
-copyright = '2026, Vicente ACOSTA and Thibault CLARA'
-author = 'Vicente ACOSTA and Thibault CLARA'
-release = 'v1.0'
+project = "mavion-drl-ppo"
+author = "Vicente ACOSTA and Thibault CLARA"
+copyright = "2026, Vicente ACOSTA and Thibault CLARA"
+release = "v1.0"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx_rtd_theme",
     "sphinx.ext.mathjax",
-    "sphinx.ext.autodoc"
+    "sphinx.ext.autodoc",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-# html_theme = 'alabaster'
-# html_static_path = ['_static']
 
 html_theme = "sphinx_rtd_theme"
 
+html_static_path = ["_static"]
+
 html_theme_options = {
-    "navigation_depth": -1,  # unlimited nested levels in sidebar
-    "collapse_navigation": False,  # keep parents expanded if you prefer
+    "navigation_depth": -1,
+    "collapse_navigation": False,
 }
